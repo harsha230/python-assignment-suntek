939515c0884f488d4a06f752cd27edf5b8606085
Harsha <78947177+harsha230@users.noreply.github.com>
2022-12-13 21:04:54 +0530

    Add files via upload

---
+++ Palindrome.py
@@ -0,0 +1,6 @@
+n = input()
+n1 = n[::-1]
+if n==n1:
+    print("Palindrome!")
+else:
+    print("Not a Palindrome")
---
+++ biggest.py
@@ -0,0 +1,7 @@
+a=int(input("Enter number 1:"))
+b=int(input("Enter number 2:"))
+
+if a>b:
+    print(a)
+else:
+    print(b)
